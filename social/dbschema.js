1a99d1c70f48bb2d1c5f2a865ae2251ea52f907e
suprio <zhoupeng@fimvisual.com>
2019-08-30 09:29:59 +0800

    Init

---
+++ social/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is a scream body",
+      createdAt: "2019-08-29T13:43:59.912Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
